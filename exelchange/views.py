8f71a52b7da681c63b7b6c40c06ba4d3859fa74f
alex123012 <makhonin.a.ru@gmail.com>
2021-02-22 01:13:16 +0300

    add absorbace-conc correlation

--- exelchange/views.py
+++ exelchange/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from .forms import ExelForm
-from .exchange import DataFrame
+# from .exchange import DataFrame
 import matplotlib.pyplot as plt
 from scipy import stats
 import os
 
+result = 0
 
 class FileFieldView(FormView):
 
@@ -15,12 +16,15 @@ class FileFieldView(FormView):
     def get(self, request, **kwargs):
         form = self.form_class()
         check(request)
-        path = path = os.path.join('exelchange', 'static', 'media')
+        path = os.path.join('exelchange', 'static', 'media')
         path = os.path.join(path, request.COOKIES.get('sessionid', ), 'graph.png')
 
         cook = os.path.join(request.COOKIES['sessionid'], 'graph.png') if os.path.exists(path) else None
+
+        global result
         return render(request, os.path.join("exelchange", "index.html"), {'form': form,
-                                                                          'img': cook})
+                                                                          'img': cook,
+                                                                          'result': result},)
 
     def post(self, request, *args, **kwargs):
         form_class = self.get_form_class()
@@ -32,26 +36,31 @@ class FileFieldView(FormView):
             a = round(appr.slope, 4)
             b = round(appr.intercept, 4)
             y1 = [a * i + b for i in x]
-            label = f'{a}*x {b}' if b < 0 else f'{a}*x + {b}'
+            label = f'y = {a}*x {b}' if b < 0 else f'{a}*x + {b}'
+            global result
+            result = [((float(i) - b) / a, float(i)) for i in request.POST['absorb'].split()]
+            # request.COOKIES['result'] = result
             fig = plt.figure(figsize=(15, 10))
             ax = fig.add_subplot(111)
             ax.set_title('Approximation curve',
                          fontsize=25,
                          color='black',
                          pad=10)
-
             ax.plot(x, y, label='initial', color='black')
             ax.plot(x, y1, label=label, color='orange')
+            for i, j in result:
+                ax.plot(i, j, 'o', color='green', markersize=20)
 
             plt.legend()
             plt.grid()
             check(request)
             fig.savefig(os.path.join('exelchange', 'static', 'media', request.COOKIES['sessionid'], 'graph.png'))
+            # print(request.COOKIES['result'])
         return redirect(request.path)
 
 
 def check(request):
-    if not request.COOKIES.get('sessionid', ) or not request.COOKIES['sessionid']:  # set session cookie
+    if not request.COOKIES.get('sessionid', '') or not request.COOKIES['sessionid']:  # set session cookie
         request.session.create()
         request.COOKIES['sessionid'] = request.session.session_key
 
@@ -64,4 +73,3 @@ def check(request):
     path = os.path.join(path, request.COOKIES.get('sessionid', ))
     if not os.path.exists(path):
         os.mkdir(path)
-
